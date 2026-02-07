88c3a6124225ae11cc4d1cc3621ce8eb7194e470
MD RAIHAN <mdraihannahid@gmail.com>
2026-02-07 23:05:46 +0530

    fix: update nodemailer config to fix timeout

--- server/mailtrap/mailtrap.ts
+++ server/mailtrap/mailtrap.ts
@@ -4,7 +4,9 @@ import dotenv from "dotenv";
 dotenv.config();
 
 export const transporter = nodemailer.createTransport({
-  service: 'gmail',
+  host: "smtp.gmail.com",
+  port: 587,
+  secure: false, // true for 465, false for other ports
   auth: {
     user: process.env.MAIL_USER,
     pass: process.env.MAIL_PASS
